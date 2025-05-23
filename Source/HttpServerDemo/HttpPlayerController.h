f4d70393b4227c64744a31aa94d07ba5a8da2724
Michael S. Kataev <lpestlname@gmail.com>
2025-05-24 00:06:47 +0300

    Added HttpPlayerController

---
+++ Source/HttpServerDemo/HttpPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HttpPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HTTPSERVERDEMO_API AHttpPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
