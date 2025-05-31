0dee3268613699150adc4f01782a3259d04d32eb
Michael S. Kataev <lpestlname@gmail.com>
2025-05-31 23:37:00 +0300

    Added base HttpInputReceiver

--- Source/HttpServerDemo/HttpPlayerController.cpp
+++ Source/HttpServerDemo/HttpPlayerController.cpp
@@ -3,3 +3,36 @@
 
 #include "HttpPlayerController.h"
 
+AHttpPlayerController::AHttpPlayerController()
+{
+	HttpInputReceiver = CreateDefaultSubobject<UHttpServer>("HttpInputReceiver");
+}
+
+void AHttpPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Run Http server
+	if (IsValid(HttpInputReceiver))
+	{
+		HttpInputReceiver->OnReceivedRequest().AddDynamic(this, &AHttpPlayerController::OnRecevedHttpInput);
+		HttpInputReceiver->StartApiServer();
+	}
+}
+
+void AHttpPlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	// Stop Http server
+	if (IsValid(HttpInputReceiver))
+	{
+		HttpInputReceiver->OnReceivedRequest().RemoveDynamic(this, &AHttpPlayerController::OnRecevedHttpInput);
+		HttpInputReceiver->StartApiServer();
+	}
+	
+	Super::EndPlay(EndPlayReason);
+}
+
+void AHttpPlayerController::OnRecevedHttpInput(const FString& RequestBody, FEndpointData EndpointData)
+{
+	
+}
--- Source/HttpServerDemo/HttpPlayerController.h
+++ Source/HttpServerDemo/HttpPlayerController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "HttpServer.h"
 #include "GameFramework/PlayerController.h"
 #include "HttpPlayerController.generated.h"
 
@@ -13,5 +14,17 @@ UCLASS()
 class HTTPSERVERDEMO_API AHttpPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AHttpPlayerController();
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION()
+	void OnRecevedHttpInput(const FString& RequestBody, FEndpointData EndpointData);
 	
+private:
+	UPROPERTY(EditAnywhere)
+	UHttpServer* HttpInputReceiver { nullptr };
 };
